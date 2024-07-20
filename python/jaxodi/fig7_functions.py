9eb48fdc9bce8315eae4b7b607251a35aa3f97e0
alserene <aserene@swin.edu.au>
2024-07-20 17:00:08 +1000

    Add files for figure 7 functions and tests.

---
+++ python/jaxodi/fig7_functions.py
@@ -0,0 +1,482 @@
+"""
+Functions for Figure 7's map.solve()
+"""
+
+from typing import Tuple
+# from collections.abc import Callable
+# from functools import partial
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+import scipy
+
+from jaxoplanet.types import Array
+
+# To generate variables ---
+# import paparazzi
+import starry
+
+from jaxodi.paparazzi.src.scripts.utils.generate import generate_data
+
+
+# Functions
+#
+#   - cho_solve
+#   - map_solve
+#   - process_inputs
+#   - solve_for_map_linear
+#   - get_default_theta
+#   - solve_bilinear
+#   - solve
+
+
+# @jax.jit
+def cho_solve(A: Array, b: Array) -> Array:
+    b_ = jax.scipy.linalg.solve_triangular(A, b, lower=True)
+    return jax.scipy.linalg.solve_triangular(jnp.transpose(A), b_, lower=False)
+
+
+# @jax.jit
+def map_solve(
+    X: Array,
+    flux: Array,
+    cho_C: float | Array,
+    mu: Array,
+    LInv: float | Array
+) -> Tuple[Array, Array]:
+    """
+    Compute the maximum a posteriori (MAP) prediction for the
+    spherical harmonic coefficients of a map given a flux timeseries.
+
+    Args:
+        X (matrix): The flux design matrix.
+        flux (array): The flux timeseries.
+        cho_C (scalar/vector/matrix): The lower cholesky factorization
+            of the data covariance.
+        mu (array): The prior mean of the spherical harmonic coefficients.
+        LInv (scalar/vector/matrix): The inverse prior covariance of the
+            spherical harmonic coefficients.
+
+    Returns:
+        The vector of spherical harmonic coefficients corresponding to the
+        MAP solution and the Cholesky factorization of the corresponding
+        covariance matrix.
+
+    """
+    # Compute C^-1 . X
+    if cho_C.ndim == 0:
+        CInvX = X / cho_C**2
+    elif cho_C.ndim == 1:
+        CInvX = jnp.dot(jnp.diag(1 / cho_C**2), X)
+    else:
+        CInvX = cho_solve(cho_C, X)
+    print(f"CInvX: {CInvX}")
+    # breakpoint()
+    # Compute W = X^T . C^-1 . X + L^-1
+    W = jnp.dot(jnp.transpose(X), CInvX)
+    print(f"W: {W}")
+    # If LInv is a scalar or a 1-dimensional array, increment the
+    # diagonal elements of W with the values from LInv.
+    if LInv.ndim == 0 or LInv.ndim == 1:
+        W = W.at[jnp.diag_indices_from(W)].set(W[jnp.diag_indices_from(W)] + LInv)
+        LInvmu = mu * LInv
+    # If LInv is a matrix, directly add LInv to W.
+    else:
+        W += LInv
+        LInvmu = jnp.dot(LInv, mu)
+    print(f"W: {W}")
+    print(f"LInvmu: {LInvmu}")
+
+    # Compute the max like y and its covariance matrix
+    cho_W = jax.scipy.linalg.cholesky(W, lower=True)
+    print(f"cho_W: {cho_W}")
+    M = cho_solve(cho_W, jnp.transpose(CInvX))
+    # print(f"M: {M}")
+    yhat = jnp.dot(M, flux) + cho_solve(cho_W, LInvmu)
+    # print(f"yhat: {yhat}")
+    ycov = cho_solve(cho_W, jnp.eye(cho_W.shape[0]))
+    # print(f"ycov: {ycov}")
+    cho_ycov = jax.scipy.linalg.cholesky(ycov, lower=True)
+    # print(f"cho_ycov: {cho_ycov}")
+
+    return yhat, cho_ycov
+
+
+def process_inputs(
+        flux,
+        nt,
+        nw,
+        nc,
+        Ny,
+        nw0,
+        nw0_,
+        S0e2i,
+        flux_err=None,
+        normalized=True,
+        baseline=None,
+        spatial_mean=None,
+        spatial_cov=None,
+        spectral_mean=None,
+        spectral_cov=None,
+        logT0=None,
+        logTf=None,
+        nlogT=None,
+    ):
+
+    # Process defaults
+    if flux_err is None:
+        flux_err = 1e-4
+    if spatial_mean is None:
+        spatial_mean = np.zeros(Ny)
+        spatial_mean[0] = 1.0
+    if spatial_cov is None:
+        spatial_cov = 1e-4
+    if spectral_mean is None:
+        spectral_mean = 1.0
+    if spectral_cov is None:
+        spectral_cov = 1e-3
+    if logT0 is None:
+        logT0 = 2
+    if logTf is None:
+        logTf = 0
+    if nlogT is None:
+        nlogT = 50
+    else:
+        nlogT = max(1, nlogT)
+    
+    # Flux must be a matrix (nt, nw)
+    # if nt == 1:
+    # else:
+    assert np.array_equal(
+        np.shape(flux), np.array([nt, nw])
+    ), "Invalid shape for `flux`."
+
+    # Flux error may be a scalar, a vector, or a matrix (nt, nw)
+    flux_err = np.array(flux_err)
+    # if flux_err.ndim == 0:
+    #     flux_err = flux_err
+    # else:
+
+    # Spatial mean may be a scalar, a vector (Ny), or a list of those
+    # Reshape it to a matrix of shape (Ny, nc)
+    if type(spatial_mean) not in (list, tuple):
+        # Use the same mean for all components
+        spatial_mean = [spatial_mean for n in range(nc)]
+    # else:
+    for n in range(nc):
+        spatial_mean[n] = np.array(spatial_mean[n])
+        assert spatial_mean[n].ndim < 2
+        spatial_mean[n] = np.reshape(
+            spatial_mean[n] * np.ones(Ny), (-1, 1)
+        )
+    spatial_mean = np.concatenate(spatial_mean, axis=-1)
+
+    # Spatial cov may be a scalar, a vector, a matrix (Ny, Ny),
+    # or a list of those. Invert it and reshape to a matrix of
+    # shape (Ny, nc) (inverse variances) or a tensor of shape
+    # (Ny, Ny, nc) (nc separate inverse covariance matrices)
+    if type(spatial_cov) not in (list, tuple):
+        # Use the same covariance for all components
+        spatial_cov = [spatial_cov for n in range(nc)]
+    # else:
+    spatial_inv_cov = [None for n in range(nc)]
+    ndim = np.array(spatial_cov[0]).ndim
+    for n in range(nc):
+        spatial_cov[n] = np.array(spatial_cov[n])
+        assert spatial_cov[n].ndim == ndim
+        if spatial_cov[n].ndim < 2:
+            spatial_inv_cov[n] = np.reshape(
+                np.ones(Ny) / spatial_cov[n], (-1, 1)
+            )
+            spatial_cov[n] = np.reshape(
+                np.ones(Ny) * spatial_cov[n], (-1, 1)
+            )
+        # else:
+    
+    # Tensor of nc (inverse) variance vectors or covariance matrices
+    spatial_cov = np.concatenate(spatial_cov, axis=-1)
+    spatial_inv_cov = np.concatenate(spatial_inv_cov, axis=-1)
+
+    # Baseline must be a vector (nt,)
+    # if baseline is not None:
+    # else:
+
+    # Spectral mean must be a scalar, a vector (nw0), or a list of those
+    # Interpolate it to the internal grid (nw0_) and reshape to (nc, nw0_)
+    if type(spectral_mean) not in (list, tuple):
+        # Use the same mean for all components
+        spectral_mean = [spectral_mean for n in range(nc)]
+    # else:
+    for n in range(nc):
+        spectral_mean[n] = np.array(spectral_mean[n])
+        assert spectral_mean[n].ndim < 2
+        spectral_mean[n] = np.reshape(
+            spectral_mean[n] * np.ones(nw0), (-1, 1)
+        )
+        spectral_mean[n] = S0e2i.dot(spectral_mean[n]).T
+    spectral_mean = np.concatenate(spectral_mean, axis=0)
+
+    # Spectral cov may be a scalar, a vector, a matrix (nw0, nw0),
+    # or a list of those. Interpolate it to the internal grid,
+    # then invert it and reshape to a matrix of
+    # shape (nc, nw0_) (inverse variances) or a tensor of shape
+    # (nc, nw0_, nw0_) (nc separate inverse covariance matrices)
+    if type(spectral_cov) not in (list, tuple):
+        # Use the same covariance for all components
+        spectral_cov = [spectral_cov for n in range(nc)]
+    # else:
+    spectral_inv_cov = [None for n in range(nc)]
+    ndim = np.array(spectral_cov[0]).ndim
+    for n in range(nc):
+        spectral_cov[n] = np.array(spectral_cov[n])
+        assert spectral_cov[n].ndim == ndim
+        if spectral_cov[n].ndim < 2:
+            if spectral_cov[n].ndim == 0:
+                cov = np.ones(nw0_) * spectral_cov[n]
+            else:
+                cov = S0e2i.dot(spectral_cov[n])
+            inv = 1.0 / cov
+            spectral_inv_cov[n] = np.reshape(inv, (1, -1))
+            spectral_cov[n] = np.reshape(cov, (1, -1))
+        # else:
+
+    # Tensor of nc (inverse) variance vectors or covariance matrices
+    spectral_cov = np.concatenate(spectral_cov, axis=0)
+    spectral_inv_cov = np.concatenate(spectral_inv_cov, axis=0)
+
+    # Spectral guess must be a scalar, a vector (nw0), or a list of those
+    # Interpolate it to the internal grid (nw0_) and reshape to (nc, nw0_)
+    # if spectral_guess is not None:
+    # else:
+
+    # Tempering schedule
+    if nlogT == 1:
+        T = np.array([10 ** logTf])
+    elif logT0 == logTf:
+        T = logTf * np.ones(nlogT)
+    else:
+        T = np.logspace(logT0, logTf, nlogT)
+
+    # Are we lucky enough to do a purely linear solve for the map?
+    linear = (not normalized) or (baseline is not None)
+
+    processed_inputs = {}
+
+    processed_inputs["flux"] = flux
+    processed_inputs["flux_err"] = flux_err
+    processed_inputs["spatial_mean"] = spatial_mean
+    processed_inputs["spatial_cov"] = spatial_cov
+    processed_inputs["spatial_inv_cov"] = spatial_inv_cov
+    processed_inputs["spectral_mean"] = spectral_mean
+    processed_inputs["spectral_cov"] = spectral_cov
+    processed_inputs["spectral_inv_cov"] = spectral_inv_cov
+    processed_inputs["T"] = T
+    processed_inputs["linear"] = linear
+
+    return processed_inputs
+
+
+def get_kT(inc, theta, veq, u):
+    """
+    Get the kernels at an array of angular phases `theta`.
+    """
+
+    # enforce_bounds()
+    # get_x()
+    # get_rT()
+    # get_kT0()
+    # F() #?
+    # right_project()
+
+    # many theano operations
+
+    pass
+
+
+def get_D_fixed_spectrum(self, inc, theta, veq, u, spectrum):
+    """
+    Return the Doppler matrix for a fixed spectrum.
+
+    This routine is heavily optimized, and can often be the fastest
+    way to compute the flux!
+
+    """
+    # # Get the convolution kernels
+    # kT = self.get_kT(inc, theta, veq, u)
+
+    # # The dot product is just a 2d convolution!
+    # product = tt.nnet.conv2d(
+    #     tt.reshape(spectrum, (self.nc, 1, 1, self.nwp)),
+    #     tt.reshape(kT, (self.nt * self.Ny, 1, 1, self.nk)),
+    #     border_mode="valid",
+    #     filter_flip=False,
+    #     input_shape=(self.nc, 1, 1, self.nwp),
+    #     filter_shape=(self.nt * self.Ny, 1, 1, self.nk),
+    # )
+    # product = tt.reshape(product, (self.nc, self.nt, self.Ny, self.nw))
+    # product = tt.swapaxes(product, 1, 2)
+    # product = tt.reshape(product, (self.Ny * self.nc, self.nt * self.nw))
+    # product = tt.transpose(product)
+    # return product
+
+    pass
+
+
+def sparse_dot(A, B):
+    """
+    Performs matrix multiplication, optimising computation time by utilising sparse matrices.
+    """
+
+    # if scipy.sparse.issparse(A):
+    #     return A.dot(B)
+    # elif scipy.sparse.issparse(B):
+    #     return (B.T.dot(A.T)).T
+    # else:
+    #     raise ValueError("At least one input must be sparse.")
+    
+    # Guess at jax versions:
+    if jax.issparse(A):
+        return A.dot(B)
+    elif jax.issparse(B):
+        return (B.T.dot(A.T)).T
+    else:
+        raise ValueError("At least one input must be sparse.")
+
+
+def design_matrix(theta, spectrum_, fix_spectrum=True, fix_map=False):
+    """
+    Return the Doppler imaging design matrix.
+
+    This matrix dots into the spectral map to yield the model for the
+    observed spectral timeseries (the ``flux``).
+    """
+    # theta = _get_default_theta(theta) # this is just undoing what get_S() did!
+
+    # # Compute the Doppler operator
+    # if fix_spectrum:
+        
+    #     # Fixed spectrum (dense)
+    #     D = get_D_fixed_spectrum(_inc, theta, _veq, _u, _spectrum)
+    #  # Interpolate to the output grid
+    # if _interp:
+    #     D = map._math.sparse_dot(_Si2eBlk, D)
+
+    # return D
+
+    pass
+
+
+# S = <603x300 sparse matrix with 1206 stored elements>
+def _get_S(theta, _angle_factor, fix_spectrum, spectrum_):
+
+    theta = theta / _angle_factor
+    dm = design_matrix(theta=theta, fix_spectrum=True)
+
+    return dm
+
+
+def solve_for_map_linear(
+        spatial_mean, spatial_inv_cov, flux_err, nt, nw, baseline_var, T, flux, S, nc, Ny,
+    ):
+    """
+    Solve for `y` linearly, given a baseline or unnormalized data.
+    """
+    # Reshape the priors
+    mu = np.reshape(np.transpose(spatial_mean), (-1))
+    if spatial_inv_cov.ndim == 2:
+        invL = np.reshape(np.transpose(spatial_inv_cov), (-1))
+
+    # Ensure the flux error is a vector
+    if flux_err.ndim == 0:
+        flux_err = flux_err * np.ones((nt, nw))
+
+    # Factorised data covariance
+    if baseline_var == 0:
+        cho_C = np.reshape(np.sqrt(T) * flux_err, (-1,))
+
+    # Unroll the data into a vector
+    flux = np.reshape(flux, (-1,))
+
+    # Get S
+    # S = _get_S()                                          # TODO: _get_S()
+
+    # load from data instead of _get_S()
+    with open("map_solve_S_input.npy", "rb") as f:
+        S = np.load(f)
+    
+    # Solve the L2 problem
+    # mean, cho_ycov = map_solve(S, flux, cho_C, mu, invL)  # TODO: map_solve()
+
+    # load from data instead of map_solve()
+    with open("map_solve_mean_output.npy", "rb") as f:
+        mean = np.load(f)
+    with open("map_solve_cho_ycov_output.npy", "rb") as f:
+        cho_ycov = np.load(f)
+
+    y = np.transpose(np.reshape(mean, (nc, Ny)))
+
+    return y, cho_ycov
+
+
+def get_default_theta(theta, _angle_factor):
+
+    return theta * _angle_factor
+
+
+def solve_bilinear(
+        flux, nt, nw, nc, Ny, nw0, nw0_, S0e2i, flux_err,               # pass to process_inputs()
+        fix_spectrum, baseline_var, S,                                  # pass to solve_for_map_linear()
+        # theta, _angle_factor, y, spectrum_, veq, inc, u, normalized,  # not being passed in yet - all for S?
+    ):
+
+    # reset() - if have a class with self attributes.
+
+    processed_inputs = process_inputs(
+        flux, nt, nw, nc, Ny, nw0, nw0_, S0e2i,
+        normalized=False, flux_err=flux_err,
+    )
+
+    linear = processed_inputs["linear"]
+    flux = processed_inputs["flux"] # doesn't change in this case
+    spatial_mean = processed_inputs["spatial_mean"]
+    spatial_inv_cov = processed_inputs["spatial_inv_cov"]
+    flux_err = processed_inputs["flux_err"]
+    # T = processed_inputs["T"]
+
+    # print(f"------ linear: {linear}")     # TODO: Remove after debugging.
+
+    if fix_spectrum:
+        if linear:
+            solution = solve_for_map_linear(
+                spatial_mean, spatial_inv_cov, flux_err, nt, nw, baseline_var, 1, flux, S, nc, Ny,
+            )
+    #     else:
+    # else:
+    
+    return solution
+
+
+def solve(
+        flux, nt, nw, nc, Ny, nw0, nw0_, S0e2i, flux_err, fix_spectrum, baseline_var, S,
+        theta, _angle_factor,
+        solver="bilinear",
+        # y, spectrum_, veq, inc, u, normalized,      # not being passed in yet - all for S?
+    ):
+    """
+    Iteratively solves the bilinear or nonlinear problem for the spatial
+    and/or spectral map given a spectral timeseries.
+    """
+
+    # Used to calculate S.
+    theta = get_default_theta(theta, _angle_factor)
+
+    if solver.lower().startswith("bi"):
+        solution = solve_bilinear(
+            flux, nt, nw, nc, Ny, nw0, nw0_, S0e2i, flux_err,                   # pass to process_inputs()
+            fix_spectrum, baseline_var, S,                                      # pass to solve_for_map_linear()
+            # theta, _angle_factor, y, spectrum_, veq, inc, u, normalized,      # not being passed in yet - all for S?
+        )
+    # else:
+    
+    return solution
---
+++ python/jaxodi/test_fig7_functions.py
@@ -0,0 +1,349 @@
+"""
+Tests for Figure 7's map.solve() and dependencies.
+
+19/7 Update
+-----------
+
+test_get_default_theta      |                   pass by call    |
+test_process_inputs         |   pass by data                    |
+test_map_solve              |                   fail by call    |
+test_solve_for_map_linear   |   pass by data    fail by call    |
+test_solve_bilinear         |   pass by data                    |
+test_solve                  |                   pass by call    |
+
+
+TODO:
+
+    - load saved data in data()
+    - create separate tests for data loads and function calls
+
+
+"""
+
+import pytest
+
+# import jax
+import jax.numpy as jnp
+import numpy as np
+import scipy
+
+# from jaxoplanet.types import Array
+from jaxoplanet.test_utils import assert_allclose
+
+# To generate variables ---
+# import paparazzi
+import starry
+from starry._core.math import greedy_linalg
+
+from jaxodi.paparazzi.src.scripts.utils.generate import generate_data
+
+from jaxodi.fig7_functions import (
+    cho_solve,
+    map_solve,
+    process_inputs,
+    solve_for_map_linear,
+    get_default_theta,
+    solve_bilinear,
+    solve
+)
+
+
+# TODO: load saved data.
+@pytest.fixture(autouse=True)
+def data():
+
+    data = generate_data()
+
+    # truth
+    # y_true = data["truths"]["y"]
+    # spectrum_true = data["truths"]["spectrum"]
+
+    # data
+    theta = data["data"]["theta"] # np.linspace(-180, 180, nt=16, endpoint=False)
+    flux = data["data"]["flux0"]
+    flux_err = data["data"]["flux0_err"]
+
+    # kwargs
+    wav = data["kwargs"]["wav"] # array (70,1)
+    wav0 = data["kwargs"]["wav0"]
+
+    # Instantiate the map
+    map = starry.DopplerMap(lazy=False, **data["kwargs"])
+    map.spectrum = data["truths"]["spectrum"]
+    for n in range(map.udeg):
+        map[1 + n] = data["props"]["u"][n]
+    
+    # Provided values
+    # ydeg = 15
+    # udeg = 2
+
+    # veq = 60000
+    # inc = 40
+
+    vsini_max = 50000
+
+    # nc = 1
+    # nt = 16
+
+    # Ny = 256
+    # Nu = 3
+    N = 324
+    nw = 70
+    # nw0 = 300
+    # nw0_ = 603
+
+    _angle_factor = np.pi / 180
+
+    # ------------------------
+    # Much code for setting S.
+    nw = 351
+    vsini_max = 50000
+    nt = 16
+    wav1 = np.min(wav)
+    wav2 = np.max(wav)
+    wavr = np.exp(0.5 * (np.log(wav1) + np.log(wav2)))
+    log_wav = np.linspace(np.log(wav1 / wavr), np.log(wav2 / wavr), nw)
+    wav_int = wavr * np.exp(log_wav)
+    interp_tol = 1e-12
+    _clight = 299792458.0  # m/s
+    dlam = log_wav[1] - log_wav[0]
+    betasini_max = vsini_max / _clight
+    hw = np.array(
+        np.ceil(
+            0.5
+            * np.abs(np.log((1 + betasini_max) / (1 - betasini_max)))
+            / dlam
+        ),
+        dtype="int32",
+    )
+    x = np.arange(0, hw + 1) * dlam
+    pad_l = log_wav[0] - hw * dlam + x[:-1]
+    pad_r = log_wav[-1] + x[1:]
+    log_wav0_int = np.concatenate([pad_l, log_wav, pad_r])
+    wav0_int = wavr * np.exp(log_wav0_int)
+    nwp = len(log_wav0_int)
+    wav0 = np.array(wav0)
+    nw0 = len(wav0)
+    S = map._get_spline_operator(wav_int, wav)
+    S[np.abs(S) < interp_tol] = 0
+    S = scipy.sparse.csr_matrix(S)
+    S = map._get_spline_operator(wav0_int, wav0)
+    S[np.abs(S) < interp_tol] = 0
+    S = scipy.sparse.csr_matrix(S)
+    S = map._get_spline_operator(wav, wav_int)
+    S[np.abs(S) < interp_tol] = 0
+    S = scipy.sparse.csr_matrix(S)
+    S = map._get_spline_operator(wav0, wav0_int)
+    S[np.abs(S) < interp_tol] = 0
+    S = scipy.sparse.csr_matrix(S)
+    # ----------------------------
+    
+    return (map, theta, _angle_factor, flux, flux_err, wav, wav0, S)
+
+
+# 19/7 -> load data     -> 
+#      -> call starry   -> pass
+def test_get_default_theta(data):
+
+    # Load the inputs.
+    map, theta, _angle_factor, flux, flux_err, wav, wav0, S = data
+
+    # Get calculated outputs.
+    calc_theta = get_default_theta(theta, _angle_factor)
+    
+    # NOTE: To get expected outputs, can either call starry function OR load saved data.
+    # Get expected outputs.
+    exp_theta = map._get_default_theta(theta)
+    # Compare calculated and expected results.
+    assert_allclose(calc_theta, exp_theta)
+
+
+# 19/7 -> load data     -> pass
+#      -> call starry   -> 
+def test_process_inputs(data):
+
+    # Load the inputs.
+    map, theta, _angle_factor, flux, flux_err, wav, wav0, S = data
+
+    # Get calculated outputs.
+    calc_processed = process_inputs(
+        flux, map.nt, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, map._S0e2i, normalized=False, flux_err=flux_err,
+    )
+
+    # NOTE: To get expected outputs, can either call starry function OR load saved data.
+    # Get expected outputs.
+    map._solver.reset()
+    map._solver.process_inputs(
+        flux, normalized=False, flux_err=flux_err,
+    )
+
+    # TODO: What is this for? Do I still need it?
+    # print(f"test_process_inputs::exp_linear: {map._solver.linear}")
+    # print(f"test_process_inputs::calc_linear: {calc_processed['linear']}")
+
+    # Compare calculated and expected results.
+    assert_allclose(map._solver.flux, calc_processed['flux'])
+    assert_allclose(map._solver.flux_err, calc_processed['flux_err'])
+    assert_allclose(map._solver.spatial_mean, calc_processed['spatial_mean'])
+    assert_allclose(map._solver.spatial_cov, calc_processed['spatial_cov'])
+    assert_allclose(map._solver.spatial_inv_cov, calc_processed['spatial_inv_cov'])
+    assert_allclose(map._solver.spectral_mean, calc_processed['spectral_mean'])
+    assert_allclose(map._solver.spectral_cov, calc_processed['spectral_cov'])
+    assert_allclose(map._solver.spectral_inv_cov, calc_processed['spectral_inv_cov'])
+    assert_allclose(map._solver.T, calc_processed['T'])
+    assert_allclose(map._solver.linear, calc_processed['linear'])
+
+
+# TODO: write test
+def test_cho_solve(data):
+
+    # cho_solve()
+    pass
+
+
+# 19/7 -> load data     -> 
+#      -> call starry   -> fail
+def test_map_solve(data):
+
+    # Load the inputs.
+    map, theta, _angle_factor, flux, flux_err, wav, wav0, S = data
+    with open("map_solve_S_input.npy", "rb") as f:
+        S = np.load(f)
+    with open("map_solve_flux_input.npy", "rb") as f:
+        flux = np.load(f)
+    with open("map_solve_cho_C_input.npy", "rb") as f:
+        cho_C = np.load(f)
+    with open("map_solve_mu_input.npy", "rb") as f:
+        mu = np.load(f)
+    with open("map_solve_invL_input.npy", "rb") as f:
+        invL = np.load(f)
+    
+    # Convert them to jax arrays?
+    S = jnp.array(S)
+    flux = jnp.array(flux)
+    cho_C = jnp.array(cho_C)
+    mu = jnp.array(mu)
+    invL = jnp.array(invL)
+
+    # Get calculated outputs.
+    calc_mean, calc_cho_ycov = map_solve(S, flux, cho_C, mu, invL)
+
+    # NOTE: To get expected outputs, can either call starry function OR load saved data.
+
+    # Get expected outputs.
+    exp_mean, exp_cho_ycov = greedy_linalg.solve(S, flux, cho_C, mu, invL)
+
+    # TODO: Try loading data. Not sure if this code is correct?
+    # # Load saved expected outputs.
+    # with open("map_solve_mean_output.npy", "rb") as f:
+    #     exp_mean = np.load(f)
+    # with open("map_solve_cho_ycov_output.npy", "rb") as f:
+    #     exp_cho_ycov = np.load(f)
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_mean, exp_mean)
+    assert_allclose(calc_cho_ycov, exp_cho_ycov)
+
+
+# 19/7 -> load data     -> pass
+#      -> call starry   -> fail
+def test_solve_for_map_linear(data):
+
+    # Load the inputs.
+    map, theta, _angle_factor, flux, flux_err, wav, wav0, S = data
+    T = 1
+    baseline_var = 0
+    S = "dummy"
+
+    # Process the inputs.
+    processed_inputs = process_inputs(
+        flux, map.nt, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, map._S0e2i,
+        normalized=False, flux_err=flux_err,
+    )
+    flux = processed_inputs["flux"] # doesn't change in this case
+    spatial_mean = processed_inputs["spatial_mean"]
+    spatial_inv_cov = processed_inputs["spatial_inv_cov"]
+    flux_err = processed_inputs["flux_err"]
+
+    # Get calculated outputs.
+    calc_y, calc_cho_ycov = solve_for_map_linear(
+        spatial_mean, spatial_inv_cov, flux_err, map.nt, map.nw, baseline_var, T, flux, S, map.nc, map.Ny,
+    )
+
+    # NOTE: To get expected outputs, can either call starry function OR load saved data.
+
+    # TODO: Fix spectrum is None error.
+    # Get expected outputs.
+    # map._solver.theta = theta * _angle_factor
+    # map._solver.process_inputs(flux)
+    # exp_y, exp_cho_ycov = map._solver.solve_for_map_linear() # --> ERROR: self.spectrum is None when design_matrix() is called
+
+    # Load saved expected outputs.
+    with open("map_solve_mean_output.npy", "rb") as f:
+        exp_mean = np.load(f)
+    with open("map_solve_cho_ycov_output.npy", "rb") as f:
+        exp_cho_ycov = np.load(f)
+    exp_y = np.transpose(np.reshape(exp_mean, (map.nc, map.Ny)))
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_y, exp_y)
+    assert_allclose(calc_cho_ycov, exp_cho_ycov)
+
+
+# 19/7 -> load data     -> pass
+#      -> call starry   -> ?
+def test_solve_bilinear(data):
+
+    # Process the inputs.
+    map, theta, _angle_factor, flux, flux_err, wav, wav0, S = data
+    fix_spectrum = True
+    baseline_var = 0
+
+    # Get calculated outputs.
+    calc_y, calc_cho_ycov = solve_bilinear(
+        flux, map._nt, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, map._S0e2i, flux_err,
+        fix_spectrum, baseline_var, S,
+    )
+
+    # NOTE: To get expected outputs, can either call starry function OR load saved data.
+    # Get expected outputs.
+    # soln = map._solver.solve_bilinear(
+    #             flux, theta, map.y, map.spectrum_, map._veq, map._inc, map._u, **kwargs   # TODO: fix inputs
+    #         )
+    # Load saved expected outputs.
+    with open("map_solve_mean_output.npy", "rb") as f:
+        exp_mean = np.load(f)
+    with open("map_solve_cho_ycov_output.npy", "rb") as f:
+        exp_cho_ycov = np.load(f)
+    exp_y = np.transpose(np.reshape(exp_mean, (map.nc, map.Ny)))
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_y, exp_y)
+    assert_allclose(calc_cho_ycov, exp_cho_ycov)
+
+
+# 19/7 -> load data     -> 
+#      -> call starry   -> pass
+def test_solve(data):
+
+    # Process the inputs.
+    map, theta, _angle_factor, flux, flux_err, wav, wav0, S = data
+    fix_spectrum = True
+    baseline_var = 0
+
+    # Get calculated outputs.
+    calc_y, calc_cho_ycov = solve(
+        flux, map.nt, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, map._S0e2i, flux_err,
+        fix_spectrum, baseline_var, S,
+        theta, _angle_factor,
+    )
+
+    # NOTE: To get expected outputs, can either call starry function OR load saved data.
+    # Get expected outputs.
+    exp_solution = map.solve(
+        flux, theta=theta, normalized=False, fix_spectrum=True, flux_err=flux_err, quiet="false"
+    )
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_y, exp_solution["y"])
+    assert_allclose(calc_cho_ycov, exp_solution["cho_ycov"])

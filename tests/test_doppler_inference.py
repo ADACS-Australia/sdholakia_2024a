573bee9944acb62a3fd2e34c410aeeacd2abd6ac
alserene <aserene@swin.edu.au>
2024-07-30 18:41:22 +1000

    Inference functions, tests and test data.

---
+++ tests/test_doppler_inference.py
@@ -0,0 +1,365 @@
+"""
+Doppler inference tests.
+"""
+
+import pytest
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+import scipy
+
+from jaxoplanet.types import Array
+from jaxoplanet.test_utils import assert_allclose
+
+# To generate variables ---
+import starry
+from starry._core.math import greedy_linalg
+
+from jaxodi.paparazzi.src.scripts.utils.generate import generate_data
+
+from jaxodi.doppler_inference import (
+    get_kT,
+    get_D_fixed_spectrum,
+    sparse_dot,
+    design_matrix,
+    _get_S,
+    cho_solve,
+    map_solve,
+    process_inputs,
+    solve_for_map_linear,
+    get_default_theta,
+    solve_bilinear,
+    solve
+)
+
+from jaxodi.doppler_forward import get_kT
+
+jax.config.update("jax_enable_x64", True)
+
+
+@pytest.fixture(autouse=True)
+def saved_input_data():
+
+    with open("map_solve_S_input.npy", "rb") as f:
+        S = jnp.load(f)
+    with open("map_solve_flux_input.npy", "rb") as f:
+        flux = jnp.load(f)
+    with open("map_solve_cho_C_input.npy", "rb") as f:
+        cho_C = jnp.load(f)
+    with open("map_solve_mu_input.npy", "rb") as f:
+        mu = jnp.load(f)
+    with open("map_solve_invL_input.npy", "rb") as f:
+        invL = jnp.load(f)
+
+    return (S, flux, cho_C, mu, invL)
+
+
+data = generate_data(u=[]) # get_kT doesn't yet have limb darkening implemented
+
+# data
+theta = data["data"]["theta"] # np.linspace(-180, 180, nt=16, endpoint=False)
+flux = data["data"]["flux0"]
+flux_err = data["data"]["flux0_err"]
+
+# kwargs
+wav = data["kwargs"]["wav"] # array (70,1)
+wav0 = data["kwargs"]["wav0"]
+
+# Instantiate the map
+map = starry.DopplerMap(lazy=False, **data["kwargs"])
+map.spectrum = data["truths"]["spectrum"]
+
+vsini_max = 50000
+nt = 16
+ydeg = 15
+udeg = 0  # Tiger has set at 0 -> no limb darkening.
+xamp = map.ops.xamp
+nwp = map.ops.nwp
+
+_angle_factor = np.pi / 180
+fix_spectrum = True
+normalized = False
+baseline_var = 0
+_S0e2i = jnp.array(map._S0e2i.toarray())
+_interp = True
+
+xamp = map.ops.xamp # (253,)
+nwp = map.ops.nwp # 603
+vsini = map.vsini # 38567.256581192356
+ydeg = map.ydeg # 15
+udeg = map.udeg # 0
+nk = map.ops.nk # 253
+nw = 351 # != map.nw = 70
+
+# veq = 60000
+# inc = 40
+# nc = 1
+# Ny = 256
+# Nu = 3
+# N = 324
+# nw = 70 # <- also 351
+# nw0 = 300
+# nw0_ = 603
+
+# ------------------------
+# Much code for setting S.
+nw = 351
+vsini_max = 50000
+wav1 = np.min(wav)
+wav2 = np.max(wav)
+wavr = np.exp(0.5 * (np.log(wav1) + np.log(wav2)))
+log_wav = jnp.linspace(np.log(wav1 / wavr), jnp.log(wav2 / wavr), nw)
+wav_int = wavr * np.exp(log_wav)
+interp_tol = 1e-12
+_clight = 299792458.0  # m/s
+dlam = log_wav[1] - log_wav[0]
+betasini_max = vsini_max / _clight
+hw = jnp.array(
+    np.ceil(
+        0.5
+        * jnp.abs(jnp.log((1 + betasini_max) / (1 - betasini_max)))
+        / dlam
+    ),
+    dtype="int32",
+)
+x = jnp.arange(0, hw + 1) * dlam
+pad_l = log_wav[0] - hw * dlam + x[:-1]
+pad_r = log_wav[-1] + x[1:]
+log_wav0_int = jnp.concatenate([pad_l, log_wav, pad_r])
+wav0_int = wavr * jnp.exp(log_wav0_int)
+wav0 = jnp.array(wav0)
+# nw0 = len(wav0)
+# nwp = len(log_wav0_int)
+
+S = map._get_spline_operator(wav_int, wav)
+S[np.abs(S) < interp_tol] = 0
+S = scipy.sparse.csr_matrix(S)
+# _Si2eTr = map._math.sparse_cast(S.T)
+_Si2eBlk = map._math.sparse_cast(
+    scipy.sparse.block_diag([S for n in range(nt)], format="csr")
+)
+S = map._get_spline_operator(wav0_int, wav0)
+S[np.abs(S) < interp_tol] = 0
+S = scipy.sparse.csr_matrix(S)
+S = map._get_spline_operator(wav, wav_int)
+S[np.abs(S) < interp_tol] = 0
+S = scipy.sparse.csr_matrix(S)
+S = map._get_spline_operator(wav0, wav0_int)
+S[np.abs(S) < interp_tol] = 0
+S = scipy.sparse.csr_matrix(S)
+
+S = jnp.array(S.toarray())
+_Si2eBlk = jnp.array(_Si2eBlk.toarray())
+
+
+
+def test_get_D_fixed_spectrum():
+
+    # Get calculated output.
+    calc_doppler_spectrum = get_D_fixed_spectrum(
+        xamp, vsini, ydeg, udeg, nk, map._inc, theta, map._spectrum, map.nc, nwp, map.nt, map.Ny, nw
+    )
+
+    # Get expected output.
+    exp_doppler_spectrum = map.ops.get_D_fixed_spectrum(map._inc, theta, map._veq, map._u, map._spectrum)
+    
+    # Compare calculated and expected results.
+    assert_allclose(calc_doppler_spectrum, exp_doppler_spectrum)
+
+
+# 29/7 -> pass unjitted, fail jitted
+def test_sparse_dot():
+
+    sparse_matrix = jnp.where(jax.random.uniform(jax.random.PRNGKey(0), (100, 100)) > 0.95, 1.0, 0.0)
+    sparse_csr_matrix = sparse_csr_matrix = scipy.sparse.csr_matrix(np.array(sparse_matrix))
+    dense_matrix = jax.random.normal(jax.random.PRNGKey(1), (100, 50))
+
+    # Get calculated output.
+    calc_D = sparse_dot(sparse_matrix, dense_matrix)
+
+    # Get expected output.
+    exp_D = map._math.sparse_dot(sparse_csr_matrix, dense_matrix)
+    
+    # Compare calculated and expected results.
+    assert_allclose(calc_D, exp_D)
+
+
+def test_design_matrix():
+
+    _interp = True
+    theta = data["data"]["theta"]
+    theta_ = theta / _angle_factor
+
+    # Get calculated output.
+    calc_dm = design_matrix(
+        theta_, _angle_factor, xamp, vsini, ydeg, udeg, nk, map._inc,
+        map._spectrum, map.nc, nwp, map.nt, map.Ny, nw, _interp, _Si2eBlk, fix_spectrum=True
+    )
+
+    # Get expected output.
+    exp_dm = map.design_matrix(theta_, fix_spectrum=True)
+    
+    # Compare calculated and expected results.
+    assert_allclose(calc_dm, exp_dm)
+
+
+# # Not sure how to call starry's equivalent function
+# def test_get_S():
+
+#     # Get calculated output.
+#     calc_S = _get_S(
+#         theta, _angle_factor, xamp, vsini, ydeg, udeg, nk, map._inc, map._spectrum,
+#         map.nc, nwp, nt, map.Ny, nw, _interp, _Si2eBlk, fix_spectrum,
+#     )
+
+#     # Get expected output.
+#     map._solver.theta = theta * _angle_factor
+#     exp_S = map._solver._get_S()
+
+#     # Compare calculated and expected results.
+#     assert_allclose(calc_S, exp_S)
+
+
+# 22/7 -> pass
+def test_get_default_theta():
+
+    # Get calculated outputs.
+    calc_theta = get_default_theta(theta, _angle_factor)
+    
+    # Get expected outputs.
+    exp_theta = map._get_default_theta(theta)
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_theta, exp_theta)
+
+
+# 22/7 -> pass
+def test_process_inputs():
+
+    # Get calculated outputs.
+    calc_processed = process_inputs(
+        flux, map.nt, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, _S0e2i, normalized=False, flux_err=flux_err,
+    )
+
+    # Get expected outputs.
+    map._solver.reset()
+    map._solver.process_inputs(
+        flux, normalized=False, flux_err=flux_err,
+    )
+
+    # Compare calculated and expected results.
+    assert_allclose(map._solver.flux, calc_processed['flux'])
+    assert_allclose(map._solver.flux_err, calc_processed['flux_err'])
+    assert_allclose(map._solver.spatial_mean, calc_processed['spatial_mean'])
+    assert_allclose(map._solver.spatial_cov, calc_processed['spatial_cov'])
+    assert_allclose(map._solver.spatial_inv_cov, calc_processed['spatial_inv_cov'])
+    assert_allclose(map._solver.spectral_mean, calc_processed['spectral_mean'])
+    assert_allclose(map._solver.spectral_cov, calc_processed['spectral_cov'])
+    assert_allclose(map._solver.spectral_inv_cov, calc_processed['spectral_inv_cov'])
+    assert_allclose(map._solver.T, calc_processed['T'])
+    assert_allclose(map._solver.linear, calc_processed['linear'])
+
+
+# 22/7 -> pass
+def test_map_solve(saved_input_data):
+
+    # Load the inputs.
+    S, flux, cho_C, mu, invL = saved_input_data
+
+    # Get calculated outputs.
+    calc_mean, calc_cho_ycov = map_solve(S, flux, cho_C, mu, invL)
+
+    # Get expected outputs.
+    exp_mean, exp_cho_ycov = greedy_linalg.solve(S, flux, cho_C, mu, invL)
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_mean, exp_mean)
+    assert_allclose(calc_cho_ycov, exp_cho_ycov)
+
+
+# 22/7 -> pass
+def test_solve_for_map_linear():
+
+    T = 1
+    flux_err = data["data"]["flux0_err"]
+
+    theta = data["data"]["theta"]
+    theta_ = theta * _angle_factor
+
+    # Process the inputs.
+    processed_inputs = process_inputs(
+        flux, map.nt, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, _S0e2i,
+        normalized=False, flux_err=flux_err,
+    )
+    # flux = processed_inputs["flux"] # doesn't change in this case
+    spatial_mean = processed_inputs["spatial_mean"]
+    spatial_inv_cov = processed_inputs["spatial_inv_cov"]
+    flux_err = processed_inputs["flux_err"]
+
+    # Get calculated outputs.
+    calc_y, calc_cho_ycov = solve_for_map_linear(
+        spatial_mean, spatial_inv_cov, flux_err, map.nt, nw, map.nw, T, flux,
+        theta_, _angle_factor, xamp, vsini, ydeg, udeg, nk, map._inc, map._spectrum,
+        map.nc, nwp, map.Ny, _interp, _Si2eBlk, fix_spectrum,
+    )
+
+    # Get expected outputs.
+    map._solver.theta = theta * _angle_factor
+    map._solver.process_inputs(flux, normalized=False, fix_spectrum=True, flux_err=flux_err)
+    map._solver.spectrum_ = map.spectrum_
+    map._solver.solve_for_map_linear()
+
+    exp_y = map._solver.y
+    exp_cho_ycov = map._solver.cho_ycov
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_y, exp_y)
+    assert_allclose(calc_cho_ycov, exp_cho_ycov)
+
+
+# 22/7 -> pass
+def test_solve_bilinear():
+
+    theta = data["data"]["theta"]
+    theta_ = theta * _angle_factor
+
+    # Get calculated outputs.
+    calc_y, calc_cho_ycov = solve_bilinear(
+        flux, nt, nw, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, _S0e2i, flux_err, normalized,
+        theta_, _angle_factor, xamp, vsini, ydeg, udeg, nk, map._inc, map._spectrum,
+        nwp, _interp, _Si2eBlk, fix_spectrum,
+    )
+
+    # Get expected outputs.
+    metadata = map._solver.solve_bilinear(
+        flux, theta_, map.y, map.spectrum_, map._veq, map._inc, map._u,
+        fix_spectrum=True,
+        flux_err=flux_err,
+        normalized=False,
+    )
+    exp_y = metadata["y"]
+    exp_cho_ycov = metadata["cho_ycov"]
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_y, exp_y)
+    assert_allclose(calc_cho_ycov, exp_cho_ycov)
+
+
+# 22/7 -> pass
+def test_solve():
+
+    # Get calculated outputs.
+    calc_y, calc_cho_ycov = solve(
+        flux, map.nt, nw, map.nw, map.nc, map.Ny, map.nw0, map.nw0_, _S0e2i, flux_err, normalized,
+        theta, _angle_factor, xamp, vsini, ydeg, udeg, nk, map._inc, map._spectrum, nwp, _interp, _Si2eBlk, fix_spectrum,
+    )
+
+    # Get expected outputs.
+    exp_solution = map.solve(
+        flux, theta=theta, normalized=False, fix_spectrum=True, flux_err=flux_err, quiet="false"
+    )
+
+    # Compare calculated and expected results.
+    assert_allclose(calc_y, exp_solution["y"])
+    assert_allclose(calc_cho_ycov, exp_solution["cho_ycov"])
